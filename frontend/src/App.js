cc8d7a95dba3d661151f35627e030676683e78a9
LeenKr <leenkkrayem@gmail.com>
2025-05-19 14:13:45 +0300

    added folders

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
+      <h1 className="text-4xl font-bold text-white underline">
+        Tailwind is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
